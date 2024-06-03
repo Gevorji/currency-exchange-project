011184bf2db353840923f6f2b7238f4fea0a75c2
Gevorji <techno9882@gmail.com>
2024-06-03 20:36:53 +0300

    WIP commit

--- app/rates_update_service.py
+++ app/rates_update_service.py
@@ -10,11 +10,17 @@ class CurrencyRatesUpdater:
     """
     Provides functionality for keeping currency rates up-to-date in application's database
     """
+    __db_details_specs = ('table_name', 'last_appeal_data_field')
 
-    def __init__(self, conn, history_of_updates_table_name):
+    def __init__(self, conn, db_details: dict):
         self.__connection: sqlite3.Connection = conn
         self.__db_cursor: sqlite3.Cursor = self.__connection.cursor()
-        self.__name_of_updates_table = history_of_updates_table_name
 
-    def update_is_needed(self):
-        self.__db_cursor.execute('SELECT last_appeal from ', (self.__name_of_updates_table,))
+        assert set(self.__db_details_specs) & set(db_details.keys()) == set(self.__db_details_specs), \
+            'One of the required database specification parameters is missing'
+
+        self.__db_details = db_details
+
+    def update_is_needed(self, source_id):
+        details = self.__db_details
+        self.__db_cursor.execute('SELECT :last_appeal_data_field from :table_name', details)
