f966004244ef9e21f524abc3f2ffa4eba9493214
Gevorji <techno9882@gmail.com>
2024-06-27 17:52:45 +0300

    WIP commit

--- web/wsgi_application.py
+++ web/wsgi_application.py
@@ -1,17 +1,37 @@
+import sys
 from urllib.parse import urlparse
 from urllib.error import URLError
+from typing import Callable, Iterable
+from http import HTTPStatus
 import re
 
 
+def http_status_enum_to_string(status: HTTPStatus):
+    return f'{status.value} {status.phrase}'
+
+
 class WSGIApplication:
-    _path_patter = re.compile('(/[a-zA-Z0-9]*)+')
+    _path_pattern = re.compile('(/[a-zA-Z0-9]*)+')
 
     def __init__(self):
         self._handler_route_map = {}
 
-    def __call__(self, env, start_response):
+    def __call__(self, env: dict, start_response: Callable):
         # path validness checking happens here (http error response)
-        pass
+        # self._get_handler == None is True -> 404 Not found
+        path = env.get('PATH_INFO')
+
+        handler = self._get_handler(path)
+
+        if not handler:
+            start_response(http_status_enum_to_string(HTTPStatus.NOT_FOUND), [])
+
+        try:
+            response: Iterable = handler(env, start_response)
+        except Exception:
+            start_response(http_status_enum_to_string(HTTPStatus.INTERNAL_SERVER_ERROR), [], sys.exc_info())
+        else:
+            return response
 
     def _get_handler(self, path):
         return self._handler_route_map.get('path')
@@ -21,10 +41,12 @@ class WSGIApplication:
             raise URLError(f'{path} is invalid path')
 
         def recorder(handler):
+            if not hasattr(handler, '__call__'):
+                raise TypeError('Handler should be callable')
             self._handler_route_map['path'] = handler
             return handler
 
         return recorder
 
     def _is_valid_path(self, path: str):
-        return True if self._path_patter.fullmatch(path) else False
+        return True if self._path_pattern.fullmatch(path) else False
--- web/wsgiserver.py
+++ web/wsgiserver.py
@@ -1,18 +1,34 @@
+import sys
 from http.server import HTTPServer, BaseHTTPRequestHandler
+from urllib.parse import urlparse
 
 
 class WSGIServerRequestHandler(BaseHTTPRequestHandler):
 
     def build_environment(self):
+        url_components = urlparse(self.path)
+
         env = {
             'REQUEST_METHOD': self.command,
             'CONTENT_LENGTH': self.headers.get('Content-Length', ''),
-            'CONTENT_TYPE': self.headers.get('Content-Type', '')
+            'CONTENT_TYPE': self.headers.get('Content-Type', ''),
+            'SCRIPT_NAME': '',
+            'PATH_INFO': self.path,
+            'QUERY_STRING': url_components.query,
+            'SERVER_NAME': 'Python http.server',
+            'SERVER_PORT': f'{self.server.server_address[0]}:{self.server.server_address[1]}',
+            'SERVER_PROTOCOL': self.protocol_version,
+            'wsgi.version': (1, 0),
+            'wsgi.url_scheme': 'http',
+            'wsgi.input': self.rfile,
+            'wsgi.errors': sys.stderr,
+            'wsgi.multithread': False,
+            'wsgi.multiprocess': False,
+            'wsgi_run_once': False
         }
 
         for kw, val in self.headers.items():
-            env_param_name = f'HTTP_{kw.upper().replace("-", "_")}'
-            env[env_param_name] = val
+            env[f'HTTP_{kw.upper().replace("-", "_")}'] = val
 
         return env
 
@@ -36,6 +52,7 @@ class WSGIServerRequestHandler(BaseHTTPRequestHandler):
                 status, response_headers = headers_stored
                 headers_sent = headers_stored
                 code, msg = status.split()
+                code = int(code)
                 if code >= 400:
                     self.send_error(code, msg)
                 else:
@@ -51,9 +68,16 @@ class WSGIServerRequestHandler(BaseHTTPRequestHandler):
         def start_response(status, headers, exc_info=None):
 
             if exc_info:
+                try:
+                    if headers_sent:
+                        raise exc_info[1].with_traceback(exc_info[2])
+                finally:
+                    exc_info = None
 
-                if headers_stored:
-                    raise exc_info[1].with_traceback(exc_info[2])
+            if headers_stored:
+                raise AssertionError(
+                    'Headers already set. Second call to start_response() should provide an exception.'
+                )
 
             headers_stored.append(status)
             headers_stored.append(headers)
@@ -64,7 +88,10 @@ class WSGIServerRequestHandler(BaseHTTPRequestHandler):
 
         try:
             for data in response:
-                write(data)
+                if data:
+                    write(data)
+            if not headers_sent:
+                write(b'')
         finally:
             if hasattr(response, 'close'):
                 response.close()
