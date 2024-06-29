852f7b1fc7cb3d45851cfc8fef858438c6bf2eb6
Gevorji <techno9882@gmail.com>
2024-06-29 20:19:35 +0300

    WIP commit

--- web/wsgi_application.py
+++ web/wsgi_application.py
@@ -1,8 +1,10 @@
 import dataclasses
+import json
 from functools import partial
 from collections import OrderedDict
+from http import HTTPStatus
 
-from .wsgi_application_base import WSGIApplication
+from wsgi_application_base import WSGIApplication, http_status_enum_to_string
 import app as coresrv
 
 
@@ -16,13 +18,23 @@ def dataclass_as_specified_dict(dataclass: dataclasses.dataclass, fields: tuple)
     return d
 
 
-currency_as_dict = partial(dataclass_as_specified_dict, fields={'id', 'full_name', 'code', 'sign'})
-
+currency_as_dict = partial(dataclass_as_specified_dict, fields=('id', 'full_name', 'code', 'sign'))
+exch_rate_as_dict = partial(dataclass_as_specified_dict, fields=('id', 'base_currency_id', 'target_currency_id', 'rate')
 
 @application.at_route('/currencies')
 class CurrenciesHandler(WSGIApplication):
 
     def doGET(self, env, start_response):
-        pass
+        query_res = [currency_as_dict(curr) for curr in coresrv.get_all_currencies()]
+        json_data = json.dumps(query_res)
+
+        start_response(
+            http_status_enum_to_string(HTTPStatus.OK),
+            (
+                ('Content-Type', 'application/json'),
+            )
+        )
+
+        yield response
 
 
