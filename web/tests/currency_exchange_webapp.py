fca5bae84f68d6e20af5c738307d83160c08c007
Gevorji <techno9882@gmail.com>
2024-07-08 18:00:26 +0300

    WIP commit

--- web/tests/currency_exchange_webapp.py
+++ web/tests/currency_exchange_webapp.py
@@ -1,8 +1,30 @@
 import unittest
 import os
 import subprocess
+from urllib.request import urlopen, Request
+from urllib.parse import urljoin
+from http import HTTPStatus
+import json
 
-subprocess.Popen(['python', 'test_server.py'])
+import app as coreapp
+
+sinf = subprocess.STARTUPINFO(dwFlags=subprocess.CREATE_NEW_CONSOLE)
+pr = subprocess.Popen(['python', 'test_server.py'], startupinfo=sinf)
+
+server_adr = 'localhost:8000'
+scheme = 'http'
+
 
 class GetAllCurrencies(unittest.TestCase):
-    pass
+
+    def test_GetCurrencies(self):
+        req = Request(server_adr + '/currencies')
+        res = urlopen(req)
+
+        code, headers, body = res.code, res.headers, res.read().decode()
+
+        correct = list(coreapp.get_all_currencies())
+
+        self.assertEqual(json.loads(body), correct)
+
+
