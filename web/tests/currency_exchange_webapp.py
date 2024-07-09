b38b58942ee95c83bc865ce1569818f25f848de1
Gevorji <techno9882@gmail.com>
2024-07-09 17:36:14 +0300

    Switching to clean python wsgi calls in webapp testing

--- web/tests/currency_exchange_webapp.py
+++ web/tests/currency_exchange_webapp.py
@@ -1,8 +1,10 @@
 import unittest
 import os
+import sys
 import subprocess
+import urllib.error
 from urllib.request import urlopen, Request
-from urllib.parse import urljoin
+from urllib.parse import urlunparse, ParseResult
 from http import HTTPStatus
 import json
 
@@ -14,12 +16,27 @@ pr = subprocess.Popen(['python', 'test_server.py'], startupinfo=sinf)
 server_adr = 'localhost:8000'
 scheme = 'http'
 
+urlbase = ('http', 'localhost:8000', '', )
+
+
+def build_url(path='', query=''):
+    return urlunparse(ParseResult('http', 'localhost:8000', path, '', query, ''))
+
+
+def do_request(req: Request):
+    try:
+        res = urlopen(req)
+    except urllib.error.HTTPError as e:
+        sys.stderr.write(f'{e.code}')
+        sys.stderr.write(f'{e.read()}')
+        return e
+
 
 class GetAllCurrencies(unittest.TestCase):
 
     def test_GetCurrencies(self):
-        req = Request(server_adr + '/currencies')
-        res = urlopen(req)
+        req = Request(build_url('/currencies'))
+        res = do_request(req)
 
         code, headers, body = res.code, res.headers, res.read().decode()
 
